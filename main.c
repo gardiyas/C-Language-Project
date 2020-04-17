16698a6f3e3cff820b6806831cbdf699385c0861
gardiyas <62068040+gardiyas@users.noreply.github.com>
2020-04-17 01:12:52 -0400

    Create main.c

---
+++ main.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "file_helper.h"
+
+
+int main(void)
+{
+	// TODO: Code the necessary logic for your solution below
+	//
+	// NOTE: Use modularity whenever possible in your design
+	//
+	// HINTS:
+	// 1) You will need to load the file data and store
+	//    it to a data structure array 
+	//    (MUST USE: provided "readFileRecord" function)
+	//
+	// 2) Create a menu system to provide the user with
+	//    the reporting options
+	//
+	// 3) Generate the user-selected report
+
+    raceManagerSystem();
+			
+	return 0;
+}
